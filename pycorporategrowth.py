fc23b6ecf085c4ee46ff9ce2d556630c7f798c89
Corsenac Pierre <pierre.corsenac@epitech.eu>
2019-11-23 10:00:35 -0500

    [SCRIPT] [SELENIULM] : adding core function to setup selenium driver and to open a specific url (here google)

--- pycorporategrowth.py
+++ pycorporategrowth.py
@@ -1,4 +1,35 @@
+import sys
+from selenium import webdriver
+
+def open_url(driver, url):
+    '''
+    Open a Specific @url with Selenium @driver
+    '''
+    try:
+        driver.get(url)
+    except:
+        print("ERROR")
+        driver.quit()
+
+def init_driver():
+    '''
+    Initiate the Selenium Web Driver
+    '''
+    try:
+        driver = webdriver.Chrome(executable_path=".//chromedriver")
+        return driver
+    except:
+        print("chromedriver not found, please place it on the root of the project's folder")
+        sys.exit(1)
+
 def main():
+    '''
+    Main function
+    '''
+    driver = init_driver()
+
+    url = "https://www.google.fr"
+    open_url(driver, url)
     print("Corporate Growth")
 
 main()
